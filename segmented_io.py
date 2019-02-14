2bb5b05e1fa41b6d1bb3099047dc5560e0c15a34
SemVan <ivan7chuk@gmail.com>
2019-02-14 23:10:17 +0300

    Reading writing segmented signals files implemented and tested

---
+++ segmented_io.py
@@ -0,0 +1,36 @@
+import numpy as np
+import csv
+
+
+def write_segmented_file(file_path,video_signal):
+    dims = video_signal.shape
+    packed_sig = pack_signal(video_signal)
+    with open(file_path, 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter = ',')
+        writer.writerow(dims)
+        for i in range(packed_sig.shape[0]):
+            for j in range(packed_sig.shape[1]):
+                writer.writerow(packed_sig[i][j])
+    return
+
+def read_segmented_file(file_name):
+    packed_sig = []
+    dims = []
+    with open(file_name, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter = ',', quoting = csv.QUOTE_NONNUMERIC)
+        dims = next(reader)
+        for row in reader:
+            packed_sig.append(row)
+    dims = [int(x) for x in dims]
+    return unpack_signal(packed_sig, dims)
+
+def pack_signal(unpacked):
+    old_shape = unpacked.shape
+    new_shape = (old_shape[0], old_shape[1], old_shape[2]*old_shape[3] )
+    packed = np.reshape(unpacked, new_shape)
+    return packed
+
+def unpack_signal(packed, dim):
+    packed = np.asarray(packed)
+    unpacked = np.reshape(packed, tuple(dim))
+    return unpacked
--- segmentor.py
+++ segmentor.py
@@ -2,7 +2,7 @@ import cv2
 from image_processor import*
 import numpy as np
 from matplotlib import pyplot as plt
-import csv
+from segmented_io import *
 
 
 float_formatter = lambda x: "%.3f" % x
@@ -11,39 +11,6 @@ np.set_printoptions(formatter={'float_kind':float_formatter})
 ver =  [6, 5, 57, 50, 33, 30, 29, 28]
 hor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11, 12, 13, 14, 15]
 
-
-def write_segmented_file(file_path,video_signal):
-    dims = video_signal.shape
-    packed_sig = pack_signal(video_signal)
-    with open(file_path, 'w') as csvfile:
-        writer = csv.writer(csvfile, delimiter = ',')
-        writer.writerow(dims)
-        for i in range(packed_sig.shape(0)):
-            for j in range(packed_sig.shape(1))
-            writer.writerow(packed_sig[i][j])
-    return
-
-def read_segmented_file(file_name):
-    packed_sig = []
-    dims = []
-    with open(file_name, 'r') as csvfile:
-        reader = csv.reader(csvfile, delimiter = ',')
-        dims = next(reader)
-        for row in reader:
-            packed_sig.append(row)
-
-    return unpack_signal(packed_sig, dims)
-
-def pack_signal(unpacked):
-    old_shape = unpacked.shape
-    new_shape = (old_shape[0], old_shape[1], old_shape[2]*old_shape[3] )
-    packed = np.reshape(sig_t, new_shape)
-    return packed
-
-def unpack_signal(packed, dim):
-    packed = np.asarray(packed)
-    unpacked = np.reshape(packed, tuple(dim))
-    return unpacked
 
 def get_segmented_video(file_name):
     cap = cv2.VideoCapture(file_name)
@@ -91,19 +58,21 @@ def get_segmented_frame(img):
 
             submats = np.asarray([x[hl_y:lr_y, hl_x:lr_x] for x in channels])
             means = np.mean(submats, axis = (1,2))
-            print(means)
+
             for k in range(len(channels)):
                 one_frame_vpg[k][len(ver)-j-2][i] = np.mean(submats[k])
     return one_frame_vpg
 
 
 frame = cv2.imread("girl.jpg")
-
-vpg = get_segmented_signal(frame, dot_array)
-print(vpg[1])
-plt.imshow(vpg[0], cmap = 'gray')
-plt.show()
-
+sig = []
+vpg = get_segmented_frame(frame)
+sig.append(vpg)
+sig = np.asarray(sig)
+file_path = "./Segmented/fuck.csv"
+write_segmented_file(file_path, sig)
+sig_r = read_segmented_file(file_path)
+print(np.isclose(sig, sig_r, atol=0.001))
 
 # face = get_face_contour(dot_array)
 # new_im = annotate_landmarks(im_grey, dot_array)
