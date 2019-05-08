58500b3112fdd1c041ba9c06ed87a3bdbc99dcdd
IvanSemchuk <ivan7chuk@gmail.com>
2019-05-08 12:11:08 +0300

    Added metrics input/output

--- full_seg_signals_processor.py
+++ full_seg_signals_processor.py
@@ -2,26 +2,34 @@ from segmented_io import *
 from phase_processor import *
 from amp_processor import *
 from general_math import *
-
+import os
 
 FILES_PATH = "./Segmented/Signals/"
+SIGNAL_FILE = "signal.csv"
 PIECE_LENGTH = 255
 
 def all_signals_processor():
-    for filename in os.listdir(VIDEO_PATH):
+    dir_list = list(os.walk(FILES_PATH))[0]
+    for dir in dir_list:
+        file_name = FILES_PATH + '/' + dir + '/' + SIGNAL_FILE
         signal = read_segmented_file(file_name)
         sig_res = one_vpg_processor(signal)
 
     return
 
 def one_vpg_processor(vpg):
-    #need to split file into windows here
-    #print shape, check the right dimension
     frame_size = vpg.shape[0]
+    full_phase = []
+    full_hr = []
+    full_snr = []
+    full_flag = []
     for i in range(frame_size-PIECE_LENGTH):
         vpg_piece_3ch = vpg[i:i+PIECE_LENGTH]
         vpg_piece_weighted = get_channels_sum(vpg_piece_3ch) #changes shape
         phase_mask = full_frame_phase_mask(vpg_piece)
-        hr_mask, snr_mask = full_frame_amp_procedure(vpg_piece)
-
-    return [phase_mask, hr_mask, snr_mask]
+        hr_mask, snr_mask, flag_mask = full_frame_amp_procedure(vpg_piece)
+        full_phase.append(phase_mask)
+        full_hr.append(hr_mask)
+        full_snr.append(snr_mask)
+        full_flag.append(flag_mask)
+    return [np.asarray(full_phase), np.asarray(full_hr), np.asarray(full_snr), np.asarray(full_flag)]
---
+++ report_io.py
@@ -0,0 +1,55 @@
+import csv
+
+PHASE_NAME = "phase.csv"
+HR_NAME = "hr.csv"
+SNR_NAME = "snr.csv"
+FLAG_NAME = "flag.csv"
+NAMES = [PHASE_NAME, HR_NAME, SNR_NAME, FLAG_NAME]
+
+def write_metrics(metrics, folder):
+    for i in range(len(results)):
+        file_name = folder + "/" + NAMES[i]
+        write_metric(metrics, file_name)
+    return
+
+def read_metrics(folder):
+    metrics = []
+    for i in range(len(NAMES)):
+        file_name = folder + "/" + NAMES[i]
+        metrics.append(read_metric(file_name))
+    return metrics
+
+def write_metric(metric, file_name):
+    """Dims are fragment-row_column"""
+    dims = metric.shape
+    packed_metric = pack_metric(metric)
+    with open(file_path, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter = ',')
+        writer.writerow(dims)
+        for i in range(packed_metric.shape[0]):
+            writer.writerow(packed_metric[i])
+    return
+
+def read_metric(file_name):
+    packed_sig = []
+    dims = []
+    with open(file_name, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter = ',', quoting = csv.QUOTE_NONNUMERIC)
+        dims = next(reader)
+        for row in reader:
+            packed_metric.append(row)
+    dims = [int(x) for x in dims]
+
+    return unpack_metric(packed_metric, dims)
+
+
+def pack_metric(unpacked):
+    old_shape = unpacked.shape
+    new_shape = (old_shape[0], old_shape[1]*old_shape[2] )
+    packed = np.reshape(unpacked, new_shape)
+    return packed
+
+def unpack_metric(packed, dim):
+    packed = np.asarray(packed)
+    unpacked = np.reshape(packed, tuple(dim))
+    return unpacked
